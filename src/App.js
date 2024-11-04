dbe5d6d98297dd6bda574f7964c3455e420b42e1
kimkwangkuk <kim12795@gmail.com>
2024-11-04 22:33:14 +0900

    변경 사항 설명

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-head">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">메뉴1</Link>
+            </li>
+            <li>
+              <Link to="/red">메뉴2</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/red" element={<RedPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>홈</h2>;
+}
+
+function RedPage() {
+  return <div style={{ backgroundColor: 'red', height: '100vh' }}>빨간 페이지</div>;
 }
 
 export default App;
